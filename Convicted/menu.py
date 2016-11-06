1b67260e5762ab8a3bb474d2c78f736990edbd16
hannahlu <HU196882@falmouth.ac.uk>
2016-11-06 16:49:09 +0000

    Changes to menu.py

--- Convicted/menu.py
+++ Convicted/menu.py
@@ -6,30 +6,35 @@ from pygame.locals import *
 pygame.init()
 
 # Set up colours
-black = (0, 0, 0)
-white = (255, 255, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # Set up window
 WINDOW_WIDTH = 1300
 WINDOW_HEIGHT = 1000
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Main Menu')
 
 # Set up background
 background = pygame.image.load('Menu Design 1.5-1.png')
 background = pygame.transform.scale(background,(WINDOW_WIDTH, WINDOW_HEIGHT))
-window.blit(background, (0, 0))
 
+# Load buttons and icons
 playbutton = pygame.image.load('1 - Play.png')
 loadbutton = pygame.image.load('2 - Load Game.png')
 optionsbutton = pygame.image.load('3 - Options.png')
 extrasbutton = pygame.image.load('4 - Extras.png')
 exitbutton = pygame.image.load('5 - Exit.png')
-window.blit(playbutton, (500, 350))
-window.blit(loadbutton, (500, 400))
-window.blit(optionsbutton, (500, 450))
-window.blit(extrasbutton, (500, 500))
-window.blit(exitbutton, (550, 550))
+icon = pygame.image.load('8bitDude.jpg')
+
+# Blit menu to screen
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+window.blit(background, (0, 0))
+pygame.display.set_icon(icon)
+pygame.display.set_caption('Main Menu')
+window.blit(playbutton, (560, 320))
+window.blit(loadbutton, (470, 380))
+window.blit(optionsbutton, (520, 430))
+window.blit(extrasbutton, (515, 480))
+window.blit(exitbutton, (570, 530))
 
 pygame.display.update()
 
