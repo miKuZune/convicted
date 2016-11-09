0a3a763bb32b1c8ba9830375fe42dd1a3510aa80
hannahlu <hannah.louise.utley@gmail.com>
2016-11-09 16:45:33 +0000

    Uploading main.py with base outline for first test level.

--- Convicted/main.py
+++ Convicted/main.py
@@ -2,68 +2,68 @@ import pygame, sys
 import os
 from pygame.locals import *
 
-# set up pygame
+# Set up pygame
 pygame.init()
 clock = pygame.time.Clock()
 
-# set up colour variables
+# Set up colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (169, 169, 169)
 
-# set up window
-WINDOW_WIDTH = 900
-WINDOW_HEIGHT = 800
+# Set up window
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 900
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Convicted')
 
-# set up player
+# Set up Road
+road = (0, 880, WINDOW_WIDTH, 60)
+
+"""Set up player"""
 class player(object):
     def __init__(self):
         self.image = pygame.image.load('runningman.png')
         self.image = pygame.transform.scale(self.image, (150, 150))
         self.x = 0
-        self.y = 650
+        self.y = 740
     # set up keyboard actions
     def handle_keys(self):
         key = pygame.key.get_pressed()
         dist = 5
-        # if key[pygame.K_DOWN]:
-            # self.y += dist
         if key[pygame.K_LEFT]:
             self.x -= dist
-        #if key[pygame.K_UP]:
-            #self.y -= dist
         if key[pygame.K_RIGHT]:
             self.x += dist
         # keep player on screen
         if self.x < 0:
             self.x = 0
-        if self.x > 670:
-            self.x = 670
+        if self.x > 740:
+            self.x = 740
         if self.y < 0:
             self.y = 0
-        if self.y > 670:
-            self.y = 670
+        if self.y > 740:
+            self.y = 740
     # draw player to surface
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
-# set up wall
+"""Set up wall"""
 class brickwall(object):
     def __init__(self):
         self.image = pygame.image.load('concretewall.png')
         self.x = 450
-        self.y = 395
+        self.y = 475
     # draw wall to surface
     def draw(self, surface):
             surface.blit(self.image, (self.x, self.y))
 
-# set up bin
+"""Set up bin"""
 class bin(object):
     def __init__(self):
         self.image = pygame.image.load('bin_192px.png')
         self.x = 250
-        self.y = 640
+        self.y = 720
     # draw bin to surface
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
@@ -82,11 +82,13 @@ while running:
         running = False
 
     playerpos.handle_keys()
+    pygame.draw.rect(window, GREY, road)
     playerpos.draw(window)
     pygame.display.update()
     window.fill(WHITE)
     wallpos.draw(window)
     binpos.draw(window)
+
     clock.tick(60)
 
 pygame.quit()
