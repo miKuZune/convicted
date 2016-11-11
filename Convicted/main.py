b57c6a2b09e7506cf3f396d156d39e595e22a96d
hannahlu <HU196882@falmouth.ac.uk>
2016-11-11 11:21:21 +0000

    Updating main.py.

--- Convicted/main.py
+++ Convicted/main.py
@@ -18,13 +18,14 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Convicted')
 
 # Set up Road
-road = (0, 880, WINDOW_WIDTH, 60)
+road = (0, 880, WINDOW_WIDTH, 40)
 
 """Set up player"""
 class player(object):
     def __init__(self):
-        self.image = pygame.image.load('runningman.png')
-        self.image = pygame.transform.scale(self.image, (150, 150))
+        self.sprite = pygame.image.load('runningman.png').convert()
+        self.sprite = pygame.transform.scale(self.sprite, (150, 150))
+        self.rect = self.sprite.get_rect()
         self.x = 0
         self.y = 740
     # set up keyboard actions
@@ -42,25 +43,27 @@ class player(object):
         # keep player on screen
         if self.x < 0:
             self.x = 0
-        if self.x > 740:
-            self.x = 740
+        if self.x > 1050:
+            self.x = 1050
         if self.y < 0:
             self.y = 0
-        if self.y > 740:
-            self.y = 740
+        if self.y > 1050:
+            self.y = 1050
     # draw player to surface
     def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
+        surface.blit(self.sprite, (self.x, self.y))
 
 """Set up wall"""
 class brickwall(object):
     def __init__(self):
-        self.image = pygame.image.load('concretewall.png')
-        self.x = 450
-        self.y = 475
+        self.sprite = pygame.image.load('concretewall_smaller.png').convert()
+        self.sprite = pygame.transform.scale(self.sprite, (138, 273))
+        self.rect = self.sprite.get_rect()
+        self.x = 860
+        self.y = 610
     # draw wall to surface
     def draw(self, surface):
-            surface.blit(self.image, (self.x, self.y))
+            surface.blit(self.sprite, (self.x, self.y))
 
 """Set up bin"""
 class bin(object):
@@ -72,18 +75,41 @@ class bin(object):
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
+"""Set up bikerack"""
+class bikerack(object):
+    def __init__(self):
+        self.image = pygame.image.load('BikeRack.png')
+        self.image = pygame.transform.scale(self.image, (400, 400))
+        self.x = 450
+        self.y = 610
+    # draw bikerack to surface
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+BrickWallRect = (138, 273, 0, 0)
+pygame.draw.rect(window, BLACK, BrickWallRect)
+PlayerRect = (150, 150, 0, 0)
+Transparent = pygame.Surface((150, 150), pygame.SRCALPHA)
+Transparent.fill((255,255,255,128))
+pygame.draw.rect(window, BLACK, PlayerRect)
+window.blit(Transparent, (0,0))
+
 # set up playerpos and wallpos variables
 playerpos = player()
 wallpos = brickwall()
 binpos = bin()
+bikerackpos = bikerack()
 
 #run game loop
 running = True
 while running:
     pressed = pygame.key.get_pressed()
+    #if pygame.sprite.spritecollide(PlayerRect, BrickWallRect, True):
+       # playerpos.x = 500
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running = False
+
 
     playerpos.handle_keys()
     pygame.draw.rect(window, GREY, road)
@@ -92,6 +118,7 @@ while running:
     window.fill(WHITE)
     wallpos.draw(window)
     binpos.draw(window)
+    bikerackpos.draw(window)
 
     clock.tick(60)
 
